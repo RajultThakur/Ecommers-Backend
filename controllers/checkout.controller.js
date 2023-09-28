6dbc19a937dc42c4f985e2961b57aeab14648fdc
Rajul Thakur <thakurajul1022@gmail.com>
2023-09-28 12:51:13 +0530

    refactor checkout route api

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -1,31 +1,70 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
+let getRawBody = require('raw-body')
+const Order = require("../models/order")
 const makePayment = async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price_data: {
-                    currency: 'inr',
-                    product_data: {
-                        name: "T-shirt"
-                    },
-                    unit_amount: 100,
+
+    const { items, shippingInfo } = req.body;
+
+    const line_items = items.map((item) => {
+        return {
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: item.name,
+                    metadata: { productId: item.productId },
                 },
-                quantity: 10
+                unit_amount: item.price * 100,
             },
-        ],
-        mode: 'payment',
-        success_url: `${process.env.FRONTEND_END_POINT}/order/success`,
-        cancel_url: `${process.env.FRONTEND_END_POINT}/cart`,
+            tax_rates: ["txr_1NtUEISCyMu7Qx9lB2pen8eo"],
+            quantity: item.quantity,
+        };
     });
 
-    // return res.json({
-    //     statue303, session.url);
+    const session = await stripe.checkout.sessions.create({
+
+        payment_method_types: ["card"],
+        success_url: `${process.env.FRONTEND_END_POINT}/order/success`,
+        cancel_url: `${process.env.FRONTEND_END_POINT}/cart`,
+        customer_email: items.userEmail,
+        client_reference_id: items[0].userID,
+        mode: "payment",
+        metadata: { shippingInfo },
+        shipping_options: [
+            {
+                shipping_rate: "shr_1NtUMISCyMu7Qx9lXDnZDlco",
+            },
+        ],
+        line_items,
+    });
 
     return res.status(303).json({
         success: true,
         url: session.url
     })
 };
+
+async function getCartItems (line_items) {
+    return new Promise((resolve, reject) => {
+        let cartItems = [];
+
+        line_items.data.forEach(async (item) => {
+            const product = await stripe.products.retrieve(item.price.product);
+            const productId = product.metadata.productId;
+
+            cartItems.push({
+                product: productId,
+                name: product.name,
+                price: item.price.unit_amount_decimal / 100,
+                quantity: item.quantity,
+                // image: product.images[0],
+            });
+
+            if (cartItems.length === line_items?.data.length) {
+                resolve(cartItems);
+            }
+        });
+    });
+}
+
 
 module.exports = { makePayment }
--- routes/checkout.route.js
+++ routes/checkout.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { makePayment } = require('../controllers/checkout.controller');
-
+const { makePayment, webhook } = require('../controllers/checkout.controller');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.post('/create-checkout-session', makePayment);
+// router.post('/webhook', bodyParser.raw({ type: 'application/json' }), webhook);
 
 module.exports = router;
